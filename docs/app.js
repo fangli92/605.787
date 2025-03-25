e26730bb2f5977efb17da7d2c1694f7c4299e276
fangli <lucyli1218@gmail.com>
2025-03-24 22:40:00 -0500

    update doc

--- docs/app.js
+++ docs/app.js
@@ -1,32 +1,62 @@
-(function () {
+(function() {
     'use strict';
 
-    angular.module('LunchCheck', [])
-        .controller('LunchCheckController', ['$scope', function ($scope) {
-            $scope.lunchItems = "";
-            $scope.message = "";
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+    .filter('customCurrency', CustomCurrencyFilter);
 
-            $scope.checkLunch = function () {
-                if (!$scope.lunchItems) {
-                    $scope.message = "Please enter data first";
-                    return;
-                }
+    // To Buy Controller
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuyCtrl = this;
+        toBuyCtrl.items = ShoppingListCheckOffService.getToBuyItems();
 
-                // Split items by comma and filter out empty items
-                var itemsArray = $scope.lunchItems.split(',');
-                var filteredItems = itemsArray.filter(item => item.length > 0); // Removing empty values
+        toBuyCtrl.buyItem = function(itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        };
+    }
 
-                var itemCount = filteredItems.length;
+    // Already Bought Controller
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var boughtCtrl = this;
+        boughtCtrl.items = ShoppingListCheckOffService.getBoughtItems();
+    }
 
-                if (itemCount === 0) {
-                    $scope.message = "Please enter data first";
-                } else if (itemCount <= 3) {
-                    $scope.message = "Enjoy!";
-                } else {
-                    $scope.message = "Too much!";
-                }
-            };
-        }]);
+    // Service to manage shopping lists
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        var toBuyItems = [
+            { name: "cookies", quantity: 10, pricePerItem: 2 },
+            { name: "apples", quantity: 5, pricePerItem: 1.5 },
+            { name: "oranges", quantity: 8, pricePerItem: 1.2 },
+            { name: "milk", quantity: 2, pricePerItem: 3 },
+            { name: "bread", quantity: 1, pricePerItem: 2.5 }
+        ];
+        var boughtItems = [];
+
+        service.buyItem = function(itemIndex) {
+            var item = toBuyItems.splice(itemIndex, 1)[0];
+            boughtItems.push(item);
+        };
+
+        service.getToBuyItems = function() {
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function() {
+            return boughtItems;
+        };
+    }
+
+    // Custom Currency Filter
+    function CustomCurrencyFilter() {
+        return function(input) {
+            return input.toFixed(2);
+        };
+    }
+
 })();
-
-
